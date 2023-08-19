aae029fb28af90446ab1c6cdc7169b9a7663784a
Fauzan Radji <triputrafauzan@gmail.com>
2023-08-19 20:35:06 +0800

    add: json-modelizer package to handle data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
-import { Card } from "../components/skeletons";
+// import { Card } from "../components/skeletons";
+import { Card } from "@/src/components";
 import { MagnifyingGlassIcon } from "@heroicons/react/24/solid";
 
+import { Site } from "@/src/models";
+
 export default function Home() {
+  const sites = Site.all();
   return (
     <>
       <header className="container mx-auto flex flex-col justify-between gap-2 p-4 md:flex-row">
@@ -20,8 +24,17 @@ export default function Home() {
         </form>
       </header>
       <main className="container mx-auto grid grid-cols-1 gap-4 p-4 md:grid-cols-2 lg:grid-cols-4">
-        {Array.from({ length: 6 }).map((_, i) => (
+        {/* {Array.from({ length: 6 }).map((_, i) => (
           <Card key={i} />
+        ))} */}
+        {sites.map((site) => (
+          <Card
+            key={site.id}
+            title={site.title}
+            poster={site.poster}
+            url={site.url}
+            description={site.description}
+          />
         ))}
       </main>
     </>
